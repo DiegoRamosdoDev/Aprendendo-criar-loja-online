8f13b65588dddfd1bfacc7192526ea8c78388c47
Diego Ramos <diegocramos07@gmail.com>
2023-09-06 14:52:57 -0300

    First commit

---
+++ js/script1.js
@@ -0,0 +1,20 @@
+class Produto {
+    constructor(){
+        this.id = 0;
+        this.nome = '';
+        this.valor = 0;
+
+    }
+
+
+    adicionar(){
+        alert('Vamos adicionar um produto!')
+        //...
+    }
+
+    excluir(){
+        alert('Item Deletado')
+    }
+}
+
+var produto = new Produto()
